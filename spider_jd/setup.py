15ff3d7826e2b6767c878f5c8d8b358c240ee1d4
Raymond <415683089@qq.com>
2016-03-01 14:00:53 +0800

    爬虫

---
+++ spider_jd/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='spider-jd',
+      version='1.0.1',
+      description='hello 123',
+      url='https://github.com/raymondchen/python-learning/tree/master/spider_jd',
+      author='jychan',
+      author_email='415683089@qq.com',
+      packages=['spider_jd'],
+      install_requires=[
+          'pybluez',
+      ],
+      zip_safe=False
+      )
