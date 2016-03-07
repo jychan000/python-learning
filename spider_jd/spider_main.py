c9736e44f27aa4a72a4f6f7ae52417874cf752d3
Raymond <415683089@qq.com>
2016-03-07 23:14:48 +0800

    spider

--- spider_jd/spider_main.py
+++ spider_jd/spider_main.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     # 打包参考https://github.com/robintibor/python-mindwave-mobile
 
     base_target_items = [1413825, 2141606] #1413825 1545675031
-    time_long = 1 #运行多久,分钟
+    time_long = 150 #运行多久,分钟
 
     obj_spider = SpiderMain()
     obj_spider.craw(base_target_items, time_long)
