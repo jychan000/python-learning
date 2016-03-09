cbe4811bdc8a77558698b6fce7d92f0eebb4368e
Raymond <415683089@qq.com>
2016-03-09 18:32:37 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='spider',
-      version='2.0.1',
+setup(name='spider-jd',
+      version='1.0.1',
       description='hello 123',
-      url='https://github.com/raymondchen/python-learning',
-      author='chenjinying',
+      url='https://github.com/raymondchen/python-learning/tree/master/spider_jd',
+      author='jychan',
       author_email='415683089@qq.com',
       packages=['spider_jd', 'analyze', 'spider'],
       install_requires=[
